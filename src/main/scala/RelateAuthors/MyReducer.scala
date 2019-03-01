14a9f4722814cd80038a29ef1e3bbf0dd1d86d24
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-02-28 19:03:55 -0600

    Added reducer

---
+++ src/main/scala/RelateAuthors/MyReducer.scala
@@ -0,0 +1,31 @@
+package RelateAuthors
+
+import java.lang
+
+import org.apache.hadoop.io.{IntWritable, Text}
+import org.apache.hadoop.mapreduce.Reducer
+import org.slf4j.{Logger, LoggerFactory}
+
+import scala.collection.JavaConverters._
+
+
+class MyReducer extends Reducer[Text, IntWritable, Text, IntWritable] {
+
+
+  val logger: Logger = LoggerFactory.getLogger(this.getClass)
+
+  override def reduce(key: Text, values: lang.Iterable[IntWritable], context: Reducer[Text, IntWritable, Text, IntWritable]#Context): Unit = {
+
+    val sum = values.asScala.foldLeft(0)(_ + _.get)
+    print("Reducer writing : ", key, new IntWritable(sum))
+    logger.info("writing Key: ", key)
+    logger.debug("writing Key: ", key)
+    context.write(key, new IntWritable(sum))
+  }
+}
+
+
+
+
+
+

4ef6f543b928846817a1ea6912a6b90431c28fa6
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-03-03 09:27:07 -0600

    Added support for the multiple input tags

--- src/main/scala/RelateAuthors/JobRunner.scala
+++ src/main/scala/RelateAuthors/JobRunner.scala
@@ -1,6 +1,6 @@
 package RelateAuthors
 
-import JHelpers.{MyInputFormat, XmlInputFormat}
+import JHelpers.XmlInputFormatWithMultipleTags
 import com.typesafe.config.ConfigFactory
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
@@ -10,16 +10,17 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat
 import org.slf4j.{Logger, LoggerFactory}
 
-import scala.xml.pull.XMLEvent
-
+/**
+  * This is the Starting point of the Map reduce Job, defining the configurations of the mapper reducer and the input and output formats
+  */
 object JobRunner {
 
   def main(args: Array[String]): Unit = {
     val logger: Logger = LoggerFactory.getLogger(this.getClass)
     val configuration = new Configuration
     val conf = ConfigFactory.load("InputFormat")
-    configuration.set("xmlinput.start", conf.getString("START_TAG"))
-    configuration.set("xmlinput.end", conf.getString("END_TAG"))
+    configuration.set("xmlinput.start", conf.getString("START_TAGS"))
+    configuration.set("xmlinput.end", conf.getString("END_TAGS"))
     configuration.set(
       "io.serializations",
       "org.apache.hadoop.io.serializer.JavaSerialization,org.apache.hadoop.io.serializer.WritableSerialization");
@@ -27,15 +28,15 @@ object JobRunner {
     println("Entered here...")
     job.setJarByClass(this.getClass)
     job.setMapperClass(classOf[MyMapper])
-    job.setInputFormatClass(classOf[XmlInputFormat])
+    job.setInputFormatClass(classOf[XmlInputFormatWithMultipleTags])
     job.setCombinerClass(classOf[MyReducer])
     job.setReducerClass(classOf[MyReducer])
     job.setOutputKeyClass(classOf[Text])
     job.setOutputValueClass(classOf[IntWritable])
-    print(args(0))
-    print(args(1))
-    FileInputFormat.addInputPath(job, new Path(args(0)))
-    FileOutputFormat.setOutputPath(job, new Path(args(1)))
+    //    println(args(0))
+    //    println(args(1))
+    FileInputFormat.addInputPath(job, new Path(args(1)))
+    FileOutputFormat.setOutputPath(job, new Path(args(2)))
     logger.debug("Setting up the Job conf....")
     System.exit(if (job.waitForCompletion(true)) 0 else 1)
 

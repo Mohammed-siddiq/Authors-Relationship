6d38ead3a2a527a8f61d11dcafb72b37e81e7309
Mohammed siddiq <iam.mohammedsiddiq@gmail.com>
2019-03-03 09:23:45 -0600

    Added unit test using scala test

--- src/test/scala/VerifyMapper.scala
+++ src/test/scala/VerifyMapper.scala
@@ -6,22 +6,30 @@ import org.scalatest.FlatSpec
 class VerifyMapper extends FlatSpec {
 
   val authorMapper = new MyMapper
-
+  val configuration = new Configuration
+  val conf = ConfigFactory.load("InputFormat")
 
   "ProcessXML Method" should "  process the passed XML and return only the cs authors" in {
-    val testXML = "    <inproceedings mdate=\"2017-05-24\" key=\"conf/icst/GrechanikHB13\">\n<author>Mark Grechanik</author>\n<author>B. M. Mainul Hossain</author>\n<author>Ugo Buy</author>\n<title>Testing Database-Centric Applications for Causes of Database Deadlocks.</title>\n<pages>174-183</pages>\n<year>2013</year>\n<booktitle>ICST</booktitle>\n<ee>https://doi.org/10.1109/ICST.2013.19</ee>\n<ee>http://doi.ieeecomputersociety.org/10.1109/ICST.2013.19</ee>\n<crossref>conf/icst/2013</crossref>\n<url>db/conf/icst/icst2013.html#GrechanikHB13</url>\n</inproceedings>"
+    val testXML = "    <inproceedings mdate=\"2017-05-24\" key=\"conf/icst/GrechanikHB13\">\n<author>Ugo Buy</author>\n<author>B. M. Mainul Hossain</author>\n<author>Mark Grechanik</author>\n<title>Testing Database-Centric Applications for Causes of Database Deadlocks.</title>\n<pages>174-183</pages>\n<year>2013</year>\n<booktitle>ICST</booktitle>\n<ee>https://doi.org/10.1109/ICST.2013.19</ee>\n<ee>http://doi.ieeecomputersociety.org/10.1109/ICST.2013.19</ee>\n<crossref>conf/icst/2013</crossref>\n<url>db/conf/icst/icst2013.html#GrechanikHB13</url>\n</inproceedings>"
     val response = authorMapper.processXML(testXML)
+
     assert(response.length == 2, "Did not return the CS authors")
+    assert(response(0) == "Mark Grechanik".toLowerCase)
+    assert(response(1) == "Ugo Buy".toLowerCase)
+
 
   }
 
-  "Conf loader" should "Load the CS authors from the conf" in {
-    val configuration = new Configuration
-    val conf = ConfigFactory.load("InputFormat")
+  "Conf loader" should "Load the confs from the conf" in {
+
     val result = conf.getStringList("UIC_CS_PROFESSORS")
     print(result)
     assert(result != Nil)
   }
+
+
+
+  //  "Generate Author Mappers"
 
 
 }
